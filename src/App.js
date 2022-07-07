a96d02c363258eb96b1999ee0ecc654141bb1696
tabrez-m <mohammed.tabrez@xoriant.com>
2022-07-07 14:25:13 +0530

    Custom Hook Fetch API

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Debounce from "./Debounce/Debounce";
+import MyFirstApp from "./MyFirstApp/Components/MyFirstApp";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import ContextAndReducer from "./Context&Reducer/ContextAndReducer";
+import FoodApp from "./foodApp/components/FoodApp";
+import MovieHome from "./MoviesHttp/MovieHome";
+import CustomHook from "./CustomHook/CustomHook";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MyFirstApp /> */}
+      {/* <Debounce /> */}
+      {/* <ContextAndReducer /> */}
+      {/* <FoodApp /> */}
+    <MovieHome />
+    {/* <CustomHook /> */}
+    </div>
+  );
+}
+
+export default App;
